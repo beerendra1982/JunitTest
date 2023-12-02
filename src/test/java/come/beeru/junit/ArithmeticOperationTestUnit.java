2464e602d6650da336b1d80b2827f6b1747187b9
beerendra1982 <Beerendra1982@gmail.com>
2023-12-02 17:54:19 +0000

    git commit --m "test"

--- src/test/java/come/beeru/junit/ArithmeticOperationTestUnit.java
+++ src/test/java/come/beeru/junit/ArithmeticOperationTestUnit.java
@@ -6,12 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArithmeticOperationTestUnit {
     @Test
-    public void testConcatWithNullInput() {
+    public void testConcatWithInput() {
         float flt1 = 10.5F;
         float flt2 = 10.5F;
         float flt3 = 10.5F;
 
         double expect = flt1+ flt2+ flt3 ;
+        float actual = ArithmeticOperation.Addition(flt1,flt2,flt3);
+        assertEquals(expect, actual);
+    }
+    @Test
+    public void testConcatWithNullInput() {
+        float flt1 = 10.5F;
+        float flt2 = 10.5F;
+        float flt3 = Float.parseFloat(null);
+
+        double expect = flt1+ flt2 ;
         float actual = ArithmeticOperation.Addition(flt1,flt2,flt3);
         assertEquals(expect, actual);
     }
